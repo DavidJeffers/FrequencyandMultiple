d3f575ef9f2394caccabcd0a60f920cc50265d74
Yuri <yuri.belorusets@gmail.com>
2022-03-11 16:00:35 -0800

    added average function

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,37 @@
-// add whatever parameters you deem necessary & write docstring
-function averagePair() {
+/** given an array of sorted numbers and a target average, returns if there is a combination of two values that average to target
+ * INPUT: array, number
+ * OUTPUT: boolean
+ */
+
+function averagePair(numbers, avg) {
+
+    // two pointers that sum and divide by two
+    // update pointer position by result moving up or down
+
+    let left = 0;
+    let right = numbers.length - 1;
+
+    if (numbers.length == 0) {
+        return false;
+    }
+
+    while (left < right) {
+
+        let avgCalc = (numbers[left] + numbers[right]) / 2;
+
+        if (avgCalc === avg) {
+            return true;
+        }
+
+        else if (avgCalc > avg) {
+            right--;
+        }
+
+        else if (avgCalc < avg) {
+            left++;
+        }
+    }
+
+    return false;
+
 }
